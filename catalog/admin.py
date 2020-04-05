83bb2cc04dc30e9b85f9ced15c8e10f4fe65757c
Edwin Kerforne <edkerforne@users.noreply.github.com>
2020-04-05 20:06:12 +0200

    Set up admin site

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import Author, Book, BookInstance, Genre, Language
 
-# Register your models here.
+admin.site.register(Genre)
+admin.site.register(Language)
+
+class BookInline(admin.StackedInline):
+    model = Book
+    extra = 0
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
+    list_display_links = ('first_name', 'last_name')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due', 'id')
+    list_filter = ('status', 'due')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due')
+        })
+    )
--- catalog/models.py
+++ catalog/models.py
@@ -43,6 +43,12 @@ class Book(models.Model):
         """Returns the URL to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
 
+    def display_genre(self):
+        """Returns a comma-separated string representation of the book's genres."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+    
+    display_genre.short_description = 'Genre'
+
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. one that can be borrowed from the library)."""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across the whole library')
